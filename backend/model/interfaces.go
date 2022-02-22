8c7d330632d92c9aa792728a1af8ad10c6f7d920
uptoy <u24to45y@gmail.com>
2022-02-22 13:15:23 +0900

    [Add]docker postgres

--- backend/model/interfaces.go
+++ backend/model/interfaces.go
@@ -1,15 +1,25 @@
 package model
 
-import "github.com/google/uuid"
+import (
+	"github.com/google/uuid"
+	"context"
+
+)
 
 // UserService defines methods the handler layer expects
 // any service it interacts with to implement
 type UserService interface {
-    Get(uid uuid.UUID) (*User, error)
+	Get(ctx context.Context, uid uuid.UUID) (*User, error)
+	Signup(ctx context.Context, u *User) error
 }
 
 // UserRepository defines methods the service layer expects
 // any repository it interacts with to implement
 type UserRepository interface {
-    FindByID(uid uuid.UUID) (*User, error)
+	FindByID(ctx context.Context, uid uuid.UUID) (*User, error)
+	Create(ctx context.Context, u *User) error
+}
+
+type TokenService interface {
+	NewPairFromUser(ctx context.Context, u *User, prevTokenID string) (*TokenPair, error)
 }
--- backend/service/user_service.go
+++ backend/service/user_service.go
@@ -2,8 +2,10 @@ package service
 
 import (
 	"backend/model"
+	"backend/model/apperrors"
 	"context"
 	"github.com/google/uuid"
+	"log"
 )
 
 // UserService acts as a struct for injecting an implementation of UserRepository
@@ -30,4 +32,24 @@ func (s *UserService) Get(ctx context.Context, uid uuid.UUID) (*model.User, erro
 	u, err := s.UserRepository.FindByID(ctx, uid)
 
 	return u, err
+}
+
+func (s *UserService) Signup(ctx context.Context, u *model.User) error {
+	pw, err := hashPassword(u.Password)
+
+	if err != nil {
+			log.Printf("Unable to signup user for email: %v\n", u.Email)
+			return apperrors.NewInternal()
+	}
+
+	// now I realize why I originally used Signup(ctx, email, password)
+	// then created a user. It's somewhat un-natural to mutate the user here
+	u.Password = pw
+	if err := s.UserRepository.Create(ctx, u); err != nil {
+			return err
+	}
+
+	// ...
+
+	return nil
 }
