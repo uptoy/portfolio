ee77bae9629b8ac4e786f609c62895b6266d48bb
uptoy <u24to45y@gmail.com>
2022-03-03 16:30:57 +0900

    [Create]product list page

--- frontend/pages/product_list.tsx
+++ frontend/pages/product_list.tsx
@@ -9,7 +9,6 @@ import Stack from '@mui/material/Stack'
 import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
 import Container from '@mui/material/Container'
-import { createTheme } from '@mui/material/styles'
 import { Layout } from 'components/organisms'
 
 const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9]
