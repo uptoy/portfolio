4a4b6fbd20f6a942219da646ab91752943f1afe8
uptoy <u24to45y@gmail.com>
2022-03-04 20:03:43 +0900

    [Add]place order page

--- frontend/pages/placeorder.tsx
+++ frontend/pages/placeorder.tsx
@@ -1,32 +1,187 @@
 import * as React from 'react'
 import type { NextPage } from 'next'
-import Container from '@mui/material/Container'
 import Typography from '@mui/material/Typography'
-import Box from '@mui/material/Box'
 import { Link } from 'components'
 import { Layout } from 'components/organisms'
+import Button from '@mui/material/Button'
+import Card from '@mui/material/Card'
+import Grid from '@mui/material/Grid'
+import List from '@mui/material/List'
+import ListItem from '@mui/material/ListItem'
+import NextLink from 'next/link'
+import CheckoutWizard from 'components/CheckoutWizard'
+import Image from 'next/image'
+import { TableContainer } from '@mui/material'
+import { TableHead } from '@mui/material'
+import { TableRow } from '@mui/material'
+import { Table } from '@mui/material'
+import TableCell from '@mui/material/TableCell'
+import { CircularProgress } from '@mui/material'
+import TableBody from '@mui/material/TableBody'
 
 const PlaceOrder: NextPage = () => {
+  const cartItems = []
+  const placeOrderHandler = () => {}
+  const shippingPrice = 100
+  const taxPrice = 100
+  const totalPrice = 100
+  const paymentMethod = 'credit'
+  const loading = false
+  const itemsPrice = 100
+  const shippingAddress = {
+    fullName: 'fullname',
+    address: 'address',
+    postalCode: 'postalCode',
+    country: 'country',
+  }
   return (
     <Layout>
-      <Container maxWidth="lg">
-        <Box
-          sx={{
-            my: 4,
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}
-        >
-          <Typography variant="h4" component="h1" gutterBottom>
-            MUI v5 + Next.js with TypeScript example
-          </Typography>
-          <Link href="/about" color="secondary">
-            Go to the about page
-          </Link>
-        </Box>
-      </Container>
+      <CheckoutWizard activeStep={3}></CheckoutWizard>
+      <Typography component="h1" variant="h1">
+        Place Order
+      </Typography>
+
+      <Grid container spacing={1}>
+        <Grid item md={9} xs={12}>
+          <Card>
+            <List>
+              <ListItem>
+                <Typography component="h2" variant="h2">
+                  Shipping Address
+                </Typography>
+              </ListItem>
+              <ListItem>
+                {shippingAddress?.fullName}, {shippingAddress?.address}, {shippingAddress?.city},{' '}
+                {shippingAddress?.postalCode}, {shippingAddress?.country}
+              </ListItem>
+            </List>
+          </Card>
+          <Card>
+            <List>
+              <ListItem>
+                <Typography component="h2" variant="h2">
+                  Payment Method
+                </Typography>
+              </ListItem>
+              <ListItem>{paymentMethod}</ListItem>
+            </List>
+          </Card>
+          <Card>
+            <List>
+              <ListItem>
+                <Typography component="h2" variant="h2">
+                  Order Items
+                </Typography>
+              </ListItem>
+              <ListItem>
+                <TableContainer>
+                  <Table>
+                    <TableHead>
+                      <TableRow>
+                        <TableCell>Image</TableCell>
+                        <TableCell>Name</TableCell>
+                        <TableCell align="right">Quantity</TableCell>
+                        <TableCell align="right">Price</TableCell>
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>
+                      {cartItems.map((item) => (
+                        <TableRow key={item._id}>
+                          <TableCell>
+                            <NextLink href={`/product/${item.slug}`} passHref>
+                              <Image
+                                src={item.image}
+                                alt={item.name}
+                                width={50}
+                                height={50}
+                              ></Image>
+                            </NextLink>
+                          </TableCell>
+
+                          <TableCell>
+                            <Link href={`/product/${item.slug}`} passHref>
+                              {item.name}
+                            </Link>
+                          </TableCell>
+                          <TableCell align="right">
+                            <Typography>{item.quantity}</Typography>
+                          </TableCell>
+                          <TableCell align="right">
+                            <Typography>${item.price}</Typography>
+                          </TableCell>
+                        </TableRow>
+                      ))}
+                    </TableBody>
+                  </Table>
+                </TableContainer>
+              </ListItem>
+            </List>
+          </Card>
+        </Grid>
+        <Grid item md={3} xs={12}>
+          <Card>
+            <List>
+              <ListItem>
+                <Typography variant="h2">Order Summary</Typography>
+              </ListItem>
+              <ListItem>
+                <Grid container>
+                  <Grid item xs={6}>
+                    <Typography>Items:</Typography>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <Typography align="right">${itemsPrice}</Typography>
+                  </Grid>
+                </Grid>
+              </ListItem>
+              <ListItem>
+                <Grid container>
+                  <Grid item xs={6}>
+                    <Typography>Tax:</Typography>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <Typography align="right">${taxPrice}</Typography>
+                  </Grid>
+                </Grid>
+              </ListItem>
+              <ListItem>
+                <Grid container>
+                  <Grid item xs={6}>
+                    <Typography>Shipping:</Typography>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <Typography align="right">${shippingPrice}</Typography>
+                  </Grid>
+                </Grid>
+              </ListItem>
+              <ListItem>
+                <Grid container>
+                  <Grid item xs={6}>
+                    <Typography>
+                      <strong>Total:</strong>
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <Typography align="right">
+                      <strong>${totalPrice}</strong>
+                    </Typography>
+                  </Grid>
+                </Grid>
+              </ListItem>
+              <ListItem>
+                <Button onClick={placeOrderHandler} variant="contained" color="primary" fullWidth>
+                  Place Order
+                </Button>
+              </ListItem>
+              {loading && (
+                <ListItem>
+                  <CircularProgress />
+                </ListItem>
+              )}
+            </List>
+          </Card>
+        </Grid>
+      </Grid>
     </Layout>
   )
 }
