b05b45d5ec8f9f81eefb6a659830194f1a1e2ebf
uptoy <u24to45y@gmail.com>
2022-03-04 18:44:39 +0900

    [Add]product detail page

---
+++ frontend/pages/product/[slug].tsx
@@ -0,0 +1,106 @@
+import { useRouter } from 'next/router'
+import NextLink from 'next/link'
+import { Link } from 'components'
+import Grid from '@mui/material/Grid'
+import List from '@mui/material/List'
+import ListItem from '@mui/material/ListItem'
+import Typography from '@mui/material/Typography'
+import Button from '@mui/material/Button'
+import Card from '@mui/material/Card'
+import Image from "next/image";
+
+const ProductDetail = () => {
+  const router = useRouter()
+  const { slug } = router.query
+  const product = {
+    name: 'name',
+    category: 'category',
+    brand: 'brand',
+    rating: 'rating',
+    numReviews: 1,
+    description: 'description',
+    price: 100,
+    countInStock: 1,
+    image:"http://placehold.jp/150x150.png"
+  }
+  // if (!product) {
+  //   return <>Product Not Found</>
+  // }
+  const addToCartHandler = () => {}
+
+  return (
+    <>
+      <Link href="/about" color="secondary">
+        back to products
+      </Link>
+      <p>ProductDetail: {slug}</p>
+      <Grid container spacing={1}>
+        <Grid item md={6} xs={12}>
+          <Image
+            src={product.image}
+            alt={product.name}
+            width={640}
+            height={640}
+            layout="responsive"
+          ></Image>
+        </Grid>
+        <Grid item md={3} xs={12}>
+          <List>
+            <ListItem>
+              <Typography component="h6" variant="h1">
+                {product.name}
+              </Typography>
+            </ListItem>
+            <ListItem>
+              <Typography>Category: {product.category}</Typography>
+            </ListItem>
+            <ListItem>
+              <Typography>Brand: {product.brand}</Typography>
+            </ListItem>
+            <ListItem>
+              <Typography>
+                Rating: {product.rating} stars ({product.numReviews} reviews)
+              </Typography>
+            </ListItem>
+            <ListItem>
+              <Typography> Description: {product.description}</Typography>
+            </ListItem>
+          </List>
+        </Grid>
+        <Grid item md={3} xs={12}>
+          <Card>
+            <List>
+              <ListItem>
+                <Grid container>
+                  <Grid item xs={6}>
+                    <Typography>Price</Typography>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <Typography>${product.price}</Typography>
+                  </Grid>
+                </Grid>
+              </ListItem>
+              <ListItem>
+                <Grid container>
+                  <Grid item xs={6}>
+                    <Typography>Status</Typography>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <Typography>{product.countInStock > 0 ? 'In stock' : 'Unavailable'}</Typography>
+                  </Grid>
+                </Grid>
+              </ListItem>
+              <ListItem>
+                <Button fullWidth variant="contained" color="primary" onClick={addToCartHandler}>
+                  Add to cart
+                </Button>
+              </ListItem>
+            </List>
+          </Card>
+        </Grid>
+      </Grid>
+    </>
+  )
+}
+
+export default ProductDetail
