8a4a091d5e6233c3e70c58dcbf66eb1ae93d01c1
uptoy <u24to45y@gmail.com>
2022-04-04 18:05:05 +0900

    [Add]auth service repository mock

---
+++ backend/service/auth_service_test.go
@@ -0,0 +1,23 @@
+package service
+
+import (
+	// "context"
+	// "fmt"
+	// "io/ioutil"
+	"testing"
+
+	// "github.com/stretchr/testify/assert"
+	// "github.com/stretchr/testify/mock"
+
+	// "backend/model"
+	// "backend/model/apperrors"
+	// "backend/model/mocks"
+	// "github.com/dgrijalva/jwt-go"
+	// "github.com/google/uuid"
+)
+
+func ForgotPassword(t *testing.T) {
+}
+
+func ResetPassword(t *testing.T) {
+}
