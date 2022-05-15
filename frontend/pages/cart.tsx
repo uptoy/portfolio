8632e5a0c3ab7050e71689f4747bf7be61f2e22f
uptoy <u24to45y@gmail.com>
2022-05-16 00:36:37 +0900

    [Update]cart page

--- frontend/pages/cart.tsx
+++ frontend/pages/cart.tsx
@@ -1,24 +1,12 @@
 import type {NextPage} from "next"
+import {useEffect} from "react"
 import Image from "next/image"
 import React from "react"
 import theme from "theme"
 import {GetServerSidePropsContext, GetServerSideProps} from "next"
 import toast from "react-hot-toast"
-import {
-  Typography,
-  Grid,
-  CardContent,
-  TableContainer,
-  Table,
-  TableHead,
-  TableRow,
-  TableCell,
-  TableBody,
-  Button,
-  Card,
-  List,
-  ListItem,
-} from "@material-ui/core"
+import {Typography, Grid, Button, List, ListItem} from "@material-ui/core"
+import Paper from "@material-ui/core/Paper"
 import {makeStyles} from "@material-ui/styles"
 import {Layout} from "components/organisms"
 import {red, common} from "@material-ui/core/colors"
@@ -28,8 +16,9 @@ import useSWR from "swr"
 import {Link} from "components"
 import RemoveCircleOutlineIcon from "@material-ui/icons/RemoveCircleOutline"
 import AddCircleOutlineIcon from "@material-ui/icons/AddCircleOutline"
-import HighlightOffIcon from "@material-ui/icons/HighlightOff"
 import {Icon} from "@material-ui/core"
+import DeleteIcon from "@material-ui/icons/Delete"
+import {useAuth} from "context/AuthContext"
 
 const BaseURL = "http://localhost:8080/api"
 
@@ -68,8 +57,24 @@ const useStyles: any = makeStyles(() => ({
       color: common.black,
     },
     "&:active": {
-      color: red[500],
+      color: common.black,
     },
+    display: "block",
+  },
+  grid: {
+    display: "grid",
+    gap: "10px",
+    gridTemplateColumns: "repeat(auto-fit, minmax(300px, 1fr))",
+  },
+  item: {
+    textAlign: "center",
+  },
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    color: theme.palette.text.secondary,
   },
 }))
 
@@ -94,6 +99,7 @@ const Cart: NextPage = ({cart}: any) => {
     fallbackData: cart,
     revalidateOnMount: true,
   })
+  const {isAuthenticated} = useAuth()
   const router = useRouter()
   const classes = useStyles()
   const fetchCartItems = data.data
@@ -170,90 +176,68 @@ const Cart: NextPage = ({cart}: any) => {
   const totalPrice: number = fetchCartItems?.reduce((total: number, cartItem: any): number => {
     return total + cartItem.quantity * cartItem.product.price
   }, 0)
+  useEffect(() => {
+    if (!isAuthenticated) {
+      router.push("/auth/signin")
+    }
+  }, [])
   return (
     <Layout>
-      <div>
-        {fetchCartItems?.length === 0 ? (
-          <div>
-            Cart is empty.
-            <Link href="/" passHref>
-              Go shopping
-            </Link>
-          </div>
-        ) : (
-          <Grid container spacing={1}>
-            <Grid item md={9} xs={12}>
-              <CardContent>
-                <TableContainer>
-                  <Table>
-                    <TableHead>
-                      <TableRow>
-                        <TableCell>Image</TableCell>
-                        <TableCell>Name</TableCell>
-                        <TableCell align="center">Quantity</TableCell>
-                        <TableCell align="center">Price</TableCell>
-                        <TableCell align="center">Action</TableCell>
-                      </TableRow>
-                    </TableHead>
-                    <TableBody>
-                      {fetchCartItems?.map((cartItem: CartItem, index: number) => (
-                        <TableRow key={index}>
-                          <TableCell>
-                            <Link href={`/product/${cartItem.product_id}`} passHref>
-                              <Image
-                                src={cartItem.product.images[0].url}
-                                alt={cartItem.product.product_name}
-                                width={50}
-                                height={50}
-                              ></Image>
-                            </Link>
-                          </TableCell>
-                          <TableCell>
-                            <Link href={`/product/${cartItem.product_id}`} passHref>
-                              <Typography>{cartItem.product.product_name}</Typography>
-                            </Link>
-                          </TableCell>
-                          <TableCell align="center">{cartItem.quantity}</TableCell>
-                          <TableCell align="center">{cartItem.product.price}</TableCell>
-                          <TableCell align="center">
-                            <Icon color="primary" className={classes.icon}>
-                              <RemoveCircleOutlineIcon onClick={() => handleDecrement(cartItem)} />
-                            </Icon>
-                            <Icon color="primary" className={classes.icon}>
-                              <HighlightOffIcon onClick={() => handleDelete(cartItem)} />
-                            </Icon>
-                            <Icon color="primary" className={classes.icon}>
-                              <AddCircleOutlineIcon onClick={() => handleIncrement(cartItem)} />
-                            </Icon>
-                          </TableCell>
-                        </TableRow>
-                      ))}
-                    </TableBody>
-                  </Table>
-                </TableContainer>
-              </CardContent>
-            </Grid>
-            <Grid item md={3} xs={12}>
-              <Card>
-                <List>
-                  <ListItem style={{justifyContent: "center"}}>
-                    <Typography variant="h6">SubTotal(${totalPrice})</Typography>
-                  </ListItem>
-                  <ListItem style={{justifyContent: "center"}}>
-                    <Button
-                      variant="contained"
-                      className={classes.checkout}
-                      color="primary"
-                      onClick={() => handleCheckOut()}
-                    >
-                      Check Out
-                    </Button>
-                  </ListItem>
-                </List>
-              </Card>
-            </Grid>
+      <div className={classes.root}>
+        <Grid container spacing={3}>
+          <Grid item xs={12} sm={6} md={9}>
+            {fetchCartItems?.map((cartItem: CartItem, index: number) => (
+              <Paper className={classes.paper} key={index}>
+                <Link href={`/product/${cartItem.product_id}`}>
+                  <div style={{display: "flex"}}>
+                    <Image
+                      src={cartItem.product?.images[0].url as string}
+                      alt={cartItem.product?.product_name}
+                      width={"100%"}
+                      height={"100%"}
+                    ></Image>
+                    <div style={{paddingLeft: 10}}>
+                      <p style={{margin: 0}}>name</p>
+                      <p>{cartItem.product?.price}</p>
+                      <p style={{color: "#007600"}}>In Stock</p>
+                    </div>
+                  </div>
+                </Link>
+                <div style={{paddingTop: 10, display: "flex"}}>
+                  <Icon color="primary" className={classes.icon}>
+                    <RemoveCircleOutlineIcon onClick={() => handleDecrement(cartItem)} />
+                  </Icon>
+                  <div style={{marginRight: 10, marginLeft: 10}}>{cartItem.quantity}</div>
+                  <Icon color="primary" className={classes.icon}>
+                    <AddCircleOutlineIcon onClick={() => handleIncrement(cartItem)} />
+                  </Icon>
+                  <Icon color="inherit" style={{marginLeft: 10}}>
+                    <DeleteIcon onClick={() => handleDelete(cartItem)} />
+                  </Icon>
+                </div>
+              </Paper>
+            ))}
           </Grid>
-        )}
+          <Grid item xs={12} sm={6} md>
+            <Paper className={classes.paper}>
+              <List>
+                <ListItem style={{justifyContent: "center"}}>
+                  <Typography variant="h6">SubTotal(${totalPrice})</Typography>
+                </ListItem>
+                <ListItem style={{justifyContent: "center"}}>
+                  <Button
+                    variant="contained"
+                    className={classes.checkout}
+                    color="primary"
+                    onClick={() => handleCheckOut()}
+                  >
+                    Check Out
+                  </Button>
+                </ListItem>
+              </List>
+            </Paper>
+          </Grid>
+        </Grid>
       </div>
     </Layout>
   )
