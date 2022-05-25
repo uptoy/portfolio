c0cac2a1480376e8fd5089dd5720abfa71303204
uptoy <u24to45y@gmail.com>
2022-05-26 00:02:28 +0900

    [Delete]Unnecessary files

--- frontend1/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ["placehold.jp", "swiperjs.com", "res.cloudinary.com", "jsonplaceholder.typicode.com"],
-  },
-}
-
-module.exports = nextConfig
