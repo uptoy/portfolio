6442d9b5a8121c7898d7c6bb8db7fef03678b876
uptoy <u24to45y@gmail.com>
2022-06-09 22:25:50 +0900

    confirm build

--- frontend/.storybook/main.js
+++ frontend/.storybook/main.js
@@ -1,27 +1,15 @@
-const path = require('path')
-
 module.exports = {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
-  framework: '@storybook/react',
-  core: {
-    builder: '@storybook/builder-webpack5'
-  },
-  staticDirs: ['../public'],
-  webpackFinal: async (config) => {
-    return {
-      ...config,
-      resolve: {
-        ...config.resolve,
-        alias: {
-          ...config.resolve.alias,
-          '@/components': path.resolve(__dirname, '../src/components'),
-          '@/lib': path.resolve(__dirname, '../src/lib'),
-          '@/models': path.resolve(__dirname, '../src/models'),
-          '@/pages': path.resolve(__dirname, '../src/pages'),
-          '@/styles': path.resolve(__dirname, '../src/styles')
-        }
-      }
-    }
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions"
+  ],
+  "framework": "@storybook/react",
+  "core": {
+    "builder": "@storybook/builder-webpack5"
   }
 }
