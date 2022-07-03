2f65ff136888f95e3e3a591a83ddd503ed023d0b
uptoy <u24to45y@gmail.com>
2022-07-03 18:14:21 +0900

    [Update]terraform cloud sql

--- terraform/cloudsql.tf
+++ terraform/cloudsql.tf
@@ -81,11 +81,18 @@ provider "google-beta" {
 resource "google_compute_instance" "apps" {
   name         = "portfolio-vm"
   machine_type = "e2-small"
+  zone         = "asia-northeast1-a"
+
 
   boot_disk {
     initialize_params {
       image = "ubuntu-os-cloud/ubuntu-1804-lts"
     }
+  }
+
+  // Local SSD disk
+  scratch_disk {
+    interface = "SCSI"
   }
 
   network_interface {
@@ -94,6 +101,11 @@ resource "google_compute_instance" "apps" {
     access_config {
       // Ephemeral IP
     }
+  }
+  service_account {
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    email  = google_service_account.default.email
+    scopes = ["cloud-platform"]
   }
 }
 
